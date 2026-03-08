aee752e4ba52b8f9b19373c9da3be07ec33e65e1
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-08 15:06:23 -0600

    include status field on vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,3 +1,5 @@
 class Vehicle < ApplicationRecord
-  validates :vin, :plate, :year, presence: true
+  enum :status, %i(active inactive in_maintenance)
+  validates :vin, :plate, :year, :status, presence: true
+  validates :year, numericality: { in: 1990..2050 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_08_192006) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_08_201036) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,5 +22,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_08_192006) do
     t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 end
