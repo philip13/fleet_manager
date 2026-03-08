2348df8000affafac1b44d415c51376cd7b35264
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-08 15:05:12 -0600

    Include valid range of years and Status Vehicle

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -4,6 +4,28 @@ RSpec.describe Vehicle, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:vin) }
     it { should validate_presence_of(:plate) }
-    it { should validate_presence_of(:year) }
+    it { should validate_presence_of(:year)}
+    it { should validate_presence_of(:status) }
+    
+    describe '#year' do
+      context 'with valid year' do
+        it 'accepts valid yeas' do
+          should allow_value(1990).for(:year)
+          should allow_value(2020).for(:year)
+          should allow_value(2050).for(:year)
+        end
+      end
+
+      context 'with invalid year' do
+        it 'rejects invalid years' do
+          should_not allow_value(1989).for(:year)
+          should_not allow_value(2051).for(:year)
+        end
+      end
+    end
+    
+    describe '#status' do
+      it { should define_enum_for(:status).with_values([:active, :inactive, :in_maintenance])}
+    end
   end
 end
