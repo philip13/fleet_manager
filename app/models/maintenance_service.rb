0332c79af8ba9e208a7c43fa55f6ca0fc2700c4c
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-08 22:47:55 -0600

    Include basic validations on maintendance_service model

--- app/models/maintenance_service.rb
+++ app/models/maintenance_service.rb
@@ -1,3 +1,6 @@
 class MaintenanceService < ApplicationRecord
   belongs_to :vehicle
+
+  validates :description, :status, :date, :cost_cents, :priority, presence: true
+  validates :cost_cents, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
