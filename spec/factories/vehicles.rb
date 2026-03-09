53a897b2eb8a19cba452a61fd1f65c53ff5ee402
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-08 22:49:13 -0600

    Update factory to use factories/vehicles

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryBot.define do
   factory :vehicle do
-    sequence(:vin) { Faker::Vehicle.vin }
-    sequence(:plate) { Faker::Vehicle.license_plate }
+    vin { Faker::Vehicle.vin }
+    plate { Faker::Vehicle.license_plate }
     brand { Faker::Vehicle.make }
     model { Faker::Vehicle.model }
     year { 2020 }
