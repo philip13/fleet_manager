dcdef723671bc4bf2079044112db2f65c1ab93a0
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-08 23:04:34 -0600

    Add enum values of status at MaintenanceService model

--- app/models/maintenance_service.rb
+++ app/models/maintenance_service.rb
@@ -1,5 +1,6 @@
 class MaintenanceService < ApplicationRecord
   belongs_to :vehicle
+  enum :status, { pending: 0, in_progress: 1, completed: 2 }
 
   validates :description, :status, :date, :cost_cents, :priority, presence: true
   validates :cost_cents, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
