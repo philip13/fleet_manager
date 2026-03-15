3a840e3c9b6d416a16bbc1e9f2d2d20bba5942ca
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-15 12:29:11 -0600

    Add Callback Models to Vehicle update status whent maintencance service status change

--- app/models/maintenance_service.rb
+++ app/models/maintenance_service.rb
@@ -7,7 +7,16 @@ class MaintenanceService < ApplicationRecord
   validates :cost_cents, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validate :date_cannot_be_in_future
   validate :complete_date_must_be_present
+
+  after_save :sync_vehicle_status
+  after_destroy :sync_vehicle_status
+
   private
+
+  def sync_vehicle_status
+    vehicle.sync_status!
+  end
+
   def date_cannot_be_in_future
     if date.present? && date > Date.today
       errors.add(:date, "can't be in the future")
--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -5,4 +5,11 @@ class Vehicle < ApplicationRecord
   validates :vin, :plate, presence: true, uniqueness: { case_sensitive: false }
   validates :year, presence: true, numericality: { only_integer: true, in: 1990..2050 }
   validates :status, presence: true
+
+  def sync_status!
+    new_status = maintenance_services.where(status: [:pending, :in_progress]).exists? ? :in_maintenance : :active
+    update(status: status)
+
+    update_column(:status, Vehicle.statuses[new_status]) unless status == new_status.to_s
+  end
 end
