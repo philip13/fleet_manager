e28b0958d4e29617e7b818a28d7497734b6d3102
Felipe Cuevas <cuevas_castillo@hotmail.com>
2026-03-15 12:26:38 -0600

    Spec: Add new validation to Vehicle, can't be status active whent a services is not compleate

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe Vehicle, type: :model do
     it { should validate_presence_of(:year) }
     it { should validate_numericality_of(:year).only_integer }
     it { should validate_presence_of(:status) }
-
-    # one vehicle could have one or more maintenance services
     it { should have_many(:maintenance_services) }
     
     describe '#year' do
@@ -34,6 +32,48 @@ RSpec.describe Vehicle, type: :model do
     
     describe '#status' do
       it { should define_enum_for(:status).with_values([:active, :inactive, :in_maintenance])}
+    end
+
+    describe '#vin' do
+      it 'validates uniqueness of vin' do
+        vehicle1 = create(:vehicle, vin: 'UNIQUEVIN123')
+        vehicle2 = build(:vehicle, vin: 'UNIQUEVIN123')
+        expect(vehicle2).not_to be_valid
+      end
+    end
+
+    describe '#plate' do
+      it 'validates uniqueness of plate' do
+        vehicle1 = create(:vehicle, plate: 'UNIQUEPLATE123')
+        vehicle2 = build(:vehicle, plate: 'UNIQUEPLATE123')
+        expect(vehicle2).not_to be_valid
+      end
+    end
+  end
+
+  describe '#sync_status!' do
+    let(:vehicle) { create(:vehicle) }
+
+    context 'when there are pending or in_progress maintenance services' do
+      before do
+        create(:maintenance_service, vehicle: vehicle, status: :pending)
+        # vehicle.sync_status!
+      end
+
+      it 'sets status to in_maintenance' do
+        expect(vehicle.status).to eq('in_maintenance')
+      end
+    end
+
+    context 'when there are no pending or in_progress maintenance services' do
+      before do
+        create(:maintenance_service, vehicle: vehicle, status: :completed, completed_at: Date.today)
+        # vehicle.sync_status!
+      end
+
+      it 'sets status to active' do
+        expect(vehicle.status).to eq('active')
+      end
     end
   end
 end
